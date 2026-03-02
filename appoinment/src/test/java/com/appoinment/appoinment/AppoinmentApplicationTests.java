d478a6e2750679402b52b31ebce9df11e38dcc61
induwaraR98 <icredu98@gmail.com>
2026-03-02 06:49:03 +0530

    create appointment

---
+++ appoinment/src/test/java/com/appoinment/appoinment/AppoinmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appoinment.appoinment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppoinmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
