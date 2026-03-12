ac7c5fa3b73ff220ac7466d1f692659eb37e5fea
0326Ashi <ashikaridi2002@gmail.com>
2026-03-12 18:32:57 +0530

    Add DTOs for payment validation

---
+++ services/payment-service/src/main/java/com/medical/payment_service/dto/ApiResponse.java
@@ -0,0 +1,10 @@
+package com.medical.payment_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+}
