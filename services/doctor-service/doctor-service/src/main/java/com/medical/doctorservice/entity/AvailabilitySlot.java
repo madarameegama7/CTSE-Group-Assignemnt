3ecda781a593ff93844676e415b9ac85c099a390
Madara Meegama <madarameegama7@gmail.com>
2026-03-15 16:00:16 +0530

    Changes to doctor service after removing builder method

--- services/doctor-service/doctor-service/src/main/java/com/medical/doctorservice/entity/AvailabilitySlot.java
+++ services/doctor-service/doctor-service/src/main/java/com/medical/doctorservice/entity/AvailabilitySlot.java
@@ -1,7 +1,10 @@
 package com.medical.doctorservice.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -12,7 +15,6 @@ import java.time.LocalTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class AvailabilitySlot {
 
     @Id
