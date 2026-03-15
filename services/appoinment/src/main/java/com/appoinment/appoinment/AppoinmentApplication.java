6def9307c51ec1f9253d589beb828502ae1b579e
induwaraR98 <icredu98@gmail.com>
2026-03-15 18:17:39 +0530

    create appoinment service

---
+++ services/appoinment/src/main/java/com/appoinment/appoinment/AppoinmentApplication.java
@@ -0,0 +1,13 @@
+package com.appoinment.appoinment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppoinmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppoinmentApplication.class, args);
+	}
+
+}
