f786a2bbdd187bbe0882267f32516d678a3bff57
0326Ashi <ashikaridi2002@gmail.com>
2026-03-20 16:59:49 +0530

    Modified update methods in payment service

---
+++ services/payment-service/src/main/java/com/medical/payment_service/dto/UpdatePaymentRequest.java
@@ -0,0 +1,23 @@
+package com.medical.payment_service.dto;
+
+import com.medical.payment_service.entity.PaymentMethod;
+import com.medical.payment_service.entity.PaymentStatus;
+import jakarta.validation.constraints.DecimalMin;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdatePaymentRequest {
+
+    @DecimalMin(value = "0.0", inclusive = false, message = "Amount must be greater than 0")
+    private BigDecimal amount;
+
+    private PaymentMethod paymentMethod;
+
+    private PaymentStatus paymentStatus;
+}
